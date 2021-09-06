620973937818c27510599ec121dfc87f435f8c9c
Andreas Hansen <creeco@gmail.com>
2021-09-06 19:31:07 +0200

    last 10 added

--- src/views/profile.js
+++ src/views/profile.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch } from "react-redux";
 import { useAuth0 } from "@auth0/auth0-react";
 import CardComp from "../components/card-comp";
@@ -11,12 +11,22 @@ const Profile = () => {
 
   const Searches = useSelector((state) => state.saveAllSearches);
 
-  const deleted = useSelector((state)=>state.hideSearch)
+  const deleted = useSelector((state) => state.hideSearch);
+
+  const [lastTen, setlastTen] = useState([]);
 
   const { user } = useAuth0();
   //Destructuring the name,picture and email form the user object.
   const { name, picture, email } = user;
   //state to store all searches made by the user in question
+
+  useEffect(() => {
+    if (Searches.length > 10) {
+      let max = Searches[Searches.length - 1];
+      let min = Searches[Searches.length - 11];
+      setlastTen(Searches.slice(min.id, max.id));
+    }
+  }, [Searches]);
 
   useEffect(() => {
     dispatch(fetchAllSearches(name));
@@ -26,7 +36,6 @@ const Profile = () => {
   return (
     <div className="text-center hero">
       <div className="row align-items-center profile-header">
-        
         <div className="col-md-2 mb-3">
           <img
             src={picture}
@@ -35,21 +44,25 @@ const Profile = () => {
           />
         </div>
         <div className="col-md text-center text-md-left">
-          <h2>{name}, so many have been deleted{deleted}</h2>
+          <h2>
+            {name}, so many have been deleted{deleted}
+          </h2>
           <p className="lead text-muted">{email}</p>
         </div>
       </div>
       <div className="row">
-      
         <pre className="col-12 text-light bg-dark p-4">
           List of all translations:
-          {Searches.map((e, index) => {
+          {lastTen.map((e, index) => {
+            //list only 10
+
             if (e.deleted) {
               return null;
             } else {
               return (
                 <div className="searchCard" id={index} key={index}>
-                  <CardComp e={e}/>
+                  {index}
+                  <CardComp e={e} />
                 </div>
               );
             }
